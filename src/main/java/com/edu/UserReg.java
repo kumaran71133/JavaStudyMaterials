1cc4b2d3e5a20390f4c90a8ce349555bb73b8e0a
kumaran71133 <kumaran71133@gmail.com>
2023-04-10 15:43:35 +0530

    initial commit

---
+++ src/main/java/com/edu/UserReg.java
@@ -0,0 +1,12 @@
+package com.edu;
+
+public class UserReg {
+
+	public static void main(String[] args) {
+		System.out.println("Hai");
+		PwdSecurityService pss = new PwdSecurityService();
+		pss.
+
+	}
+
+}
